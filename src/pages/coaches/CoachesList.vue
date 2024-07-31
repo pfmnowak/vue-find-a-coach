2462ebc01965067f3405cffe3f97c41adbc4a67c
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2024-07-31 21:11:09 +0200

    Add store

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,10 +1,30 @@
 <template>
   <section>FILTER</section>
-  <section>LIST OF COACHES</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as a Coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName + ' ' + coach.lastName }}
+      </li>
+    </ul>
+    <h3 v-else>No coaches found.</h3>
+  </section>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
 </script>
 
 <style scoped></style>
