b01df61eef2b4c601142a02a31c6b0af4ab671fd
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2024-08-05 21:45:37 +0200

    Add data cacheing, update not found page

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -26,7 +26,11 @@ export default {
 
     context.commit('registerCoach', { ...coachData, id: userId });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(`${FIREBASE_DB_URL}coaches.json`);
     const responseData = await response.json();
 
@@ -44,5 +48,6 @@ export default {
     });
 
     context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp', coaches);
   },
 };
