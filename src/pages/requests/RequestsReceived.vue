8698541e5ced09a4c06e4ea8a00cff7149a4d447
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2024-08-05 21:18:19 +0200

    Store requests in the DB

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
   <section>
+    <base-dialog :show="!!error" title="An error occured!" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
     <base-card>
       <header>
         <h2>Requests Received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <div v-if="isLoading"><base-spinner></base-spinner></div>
+      <ul v-else-if="hasRequests && !isLoading">
         <request-item
           v-for="request in receivedRequests"
           :key="request.id"
@@ -25,6 +29,12 @@ export default {
   components: {
     RequestItem,
   },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
@@ -32,6 +42,23 @@ export default {
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
     },
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    },
+  },
+  created() {
+    this.loadRequests();
   },
 };
 </script>
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,56 @@
+import { FIREBASE_DB_URL } from '../../../constants';
+
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const response = await fetch(
+      `${FIREBASE_DB_URL}requests${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to send request!'
+      );
+      throw error;
+    }
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
     context.commit('addRequest', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const response = await fetch(`${FIREBASE_DB_URL}requests${coachId}.json`);
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to fetch request!'
+      );
+      throw error;
+    }
+
+    const requests = Object.entries(responseData).map(([key, requestValue]) => {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: requestValue.userEmail,
+        message: requestValue.message,
+      };
+
+      return request;
+    });
+
+    context.commit('setRequests', requests);
   },
 };
