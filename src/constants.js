13df3f2187d389588c8057a219383ffee38af4de
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2024-08-06 19:17:47 +0200

    Add signup

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 export const FIREBASE_DB_URL =
   'https://vue-http-demo-7e623-default-rtdb.europe-west1.firebasedatabase.app/';
+export const API_KEY = 'AIzaSyAPU15ErPl-ocOAILAkCseey3DWoEueRvA';
+export const FIREBASE_AUTH_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
