e282191a4b87dcf90b32d3c14822a6fb556db0e8
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2024-11-24 19:31:25 +0100

    Create .env file

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
-export const FIREBASE_DB_URL =
-  'https://vue-http-demo-7e623-default-rtdb.europe-west1.firebasedatabase.app/';
-const API_KEY = 'AIzaSyAPU15ErPl-ocOAILAkCseey3DWoEueRvA';
-const FIREBASE_AUTH_BASE_URL = `https://identitytoolkit.googleapis.com/v1/accounts`;
+export const FIREBASE_DB_URL = process.env.VUE_APP_FIREBASE_DB_URL;
+const API_KEY = process.env.VUE_APP_FIREBASE_API_KEY;
+const FIREBASE_AUTH_BASE_URL = process.env.VUE_APP_FIREBASE_AUTH_BASE_URL;
 export const FIREBASE_AUTH_SIGNUP_URL = `${FIREBASE_AUTH_BASE_URL}:signUp?key=${API_KEY}`;
 export const FIREBASE_AUTH_LOGIN_URL = `${FIREBASE_AUTH_BASE_URL}:signInWithPassword?key=${API_KEY}`;
